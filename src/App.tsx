abcf70ac38b835fc5f3fc9c72dd2f4626cc211a7
Mark Pokrovsky <solexofficial@yandex.ru>
2023-01-31 14:42:24 +0300

    ✨ split chunks page, add lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./index.scss";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 type Props = {};
 
@@ -10,10 +11,13 @@ const App = (props: Props) => {
     <div className="app">
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Routes>
-        <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/"} element={<MainPage />} />
-      </Routes>
+
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
