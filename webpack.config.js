461ff52a0c002f71ca3175eeb693cfc2070eaf39
Mark Pokrovsky <solexofficial@yandex.ru>
2023-01-31 10:24:22 +0300

    ✨add options webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: path.resolve(__dirname, "src", "index.js"),
+
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
+    clean: true,
   },
 };
