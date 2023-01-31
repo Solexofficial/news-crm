9fa9b625f4c526a3c6e5ef933ebfe24bd1195a29
Mark Pokrovsky <solexofficial@yandex.ru>
2023-01-31 14:17:33 +0300

    📦install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import Counter from "./components/counter";
+import { Routes, Route, Link } from "react-router-dom";
 import "./index.scss";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 type Props = {};
 
 const App = (props: Props) => {
   return (
     <div className="app">
-      hello
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Routes>
+        <Route path={"/about"} element={<AboutPage />} />
+        <Route path={"/"} element={<MainPage />} />
+      </Routes>
     </div>
   );
 };
