6dbcbdc4f783c456141acf5cc37dcdacf99f81ce
Mark Pokrovsky <solexofficial@yandex.ru>
2023-02-12 01:50:15 +0300

    🛠️fix: eslint maxLen and ignore comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     '@typescript-eslint/naming-convention': 'warn',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { code: 100, ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
