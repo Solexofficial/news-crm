16eca0ecba47fcf8a9c0defd8206e336daff9d31
Mark Pokrovsky <solexofficial@yandex.ru>
2023-02-11 20:02:28 +0300

    ✨add babel-loader, i18extractor

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,28 @@ import { RuleSetRule } from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["en", "ru"],
+              keyAsDefaultValue: false,
+              saveMissing: true,
+              outputPath: "public/locales/{{locale}}/{{ns}}.json",
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
@@ -40,5 +62,5 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [babelLoader, fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
