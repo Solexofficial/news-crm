434a5c92981d0dfd4fd464dd91661da4e187d443
Mark Pokrovsky <solexofficial@yandex.ru>
2023-02-18 14:26:45 +0300

    🧪tests: add Button test

---
+++ src/shared/ui/Button/ui/Button.test.tsx
@@ -0,0 +1,9 @@
+import { Button } from 'shared/ui/Button';
+import { render, screen } from '@testing-library/react';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    render(<Button>test</Button>);
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+});
