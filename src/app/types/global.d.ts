62b105ad7cec7de824683697b1fbe65cea64cc06
Mark Pokrovsky <solexofficial@yandex.ru>
2023-02-21 00:23:58 +0300

    🛠️refactor: declare scss

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,11 +1,7 @@
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
+declare module '*.scss' {
+  type ClassNames = Record<string, string>;
+  const classNames: ClassNames;
+  export = classNames;
 }
 
 declare module '*.png';
