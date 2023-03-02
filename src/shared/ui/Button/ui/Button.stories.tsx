f2117470b6309a9ef58369af9d7ae27b9f8ea478
Mark Pokrovsky <solexofficial@yandex.ru>
2023-03-02 23:42:24 +0300

    ✨add btn sizes

--- src/shared/ui/Button/ui/Button.stories.tsx
+++ src/shared/ui/Button/ui/Button.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { ButtonTheme } from '..';
-import { Button } from './Button';
+import { Button, ButtonSize } from './Button';
 
 export default {
   title: 'shared/Button',
@@ -36,3 +36,66 @@ Outline.args = {
 };
 
 Outline.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OutlineSizeL = Template.bind({});
+
+OutlineSizeL.args = {
+  children: 'Outline',
+  variant: ButtonTheme.OUTLINE,
+  size: ButtonSize.L,
+};
+
+OutlineSizeL.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OutlineSizeXL = Template.bind({});
+
+OutlineSizeXL.args = {
+  children: 'Outline',
+  variant: ButtonTheme.OUTLINE,
+  size: ButtonSize.XL,
+};
+
+OutlineSizeXL.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const BackgroundTheme = Template.bind({});
+BackgroundTheme.args = {
+  children: 'Text',
+  variant: ButtonTheme.BACKGROUND,
+};
+
+export const BackgroundInvertedTheme = Template.bind({});
+BackgroundInvertedTheme.args = {
+  children: 'Text',
+  variant: ButtonTheme.BACKGROUND_INVERTED,
+};
+
+export const Square = Template.bind({});
+BackgroundInvertedTheme.args = {
+  children: '>',
+  variant: ButtonTheme.BACKGROUND,
+  square: true,
+};
+
+export const SquareSizeM = Template.bind({});
+SquareSizeM.args = {
+  children: '>',
+  variant: ButtonTheme.BACKGROUND,
+  square: true,
+  size: ButtonSize.M,
+};
+
+export const SquareSizeL = Template.bind({});
+SquareSizeL.args = {
+  children: '>',
+  variant: ButtonTheme.BACKGROUND,
+  square: true,
+  size: ButtonSize.L,
+};
+
+export const SquareSizeXL = Template.bind({});
+SquareSizeXL.args = {
+  children: '>',
+  variant: ButtonTheme.BACKGROUND,
+  square: true,
+  size: ButtonSize.XL,
+};
