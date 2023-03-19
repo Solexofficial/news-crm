496c689f09d4bb2d2c76c05fde3a9f9e1ed9ac93
Mark Pokrovsky <solexofficial@yandex.ru>
2023-03-20 00:18:49 +0300

    feat: :sparkles: add input stories

---
+++ src/shared/ui/Input/ui/Input.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Input } from '..';
+
+export default {
+  title: 'shared/Input',
+  component: Input,
+  argTypes: {},
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  value: 'Text',
+  placeholder: 'Placeholder',
+};
